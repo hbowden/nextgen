720def8d161b229facb2e650a9c975832a214b3d
Harrison <2trill2spill@gmail.com>
2016-03-22 10:54:00 -0500

    Add new syscall entries.

---
+++ src/syscall/mac/entry_setuid.c
@@ -0,0 +1,31 @@
+
+
+/**
+ * Copyright (c) 2016, Harrison Bowden, Minneapolis, MN
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright notice 
+ * and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH 
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY 
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, 
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ **/
+
+#include "syscall_list.h"
+
+struct syscall_entry entry_setuid = {
+
+    .name_of_syscall = "setuid",
+    .syscall_symbol = SYS_setuid,
+    .number_of_args = 1,
+    .status = ON,
+    .requires_root = NX_NO,
+    .need_alarm = NX_NO,
+
+    .arg_type_index[FIRST_ARG] = INT,
+    .get_arg_index[FIRST_ARG] = &generate_int
+};
