37814f3f70bc64b18316e0bbc442abf1577372ec
nah <2trill2spill@gmail.com>
2016-06-13 21:04:25 -0500

    Dont use echo.

--- get-aclocal.sh
+++ get-aclocal.sh
@@ -2,4 +2,4 @@
 
 wget http://ftp.gnu.org/gnu/automake/automake-1.5.tar.gz && \
 tar -xvf automake-1.5.tar.gz && cd automake-1.5 && ./configure &&
-make -j2 && sudo make install && echo aclocal --version
+make -j2 && sudo make install && aclocal --version
