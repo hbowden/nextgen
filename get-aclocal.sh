1fbd34930726065053f1d17434c6881ee8632a8a
nah <2trill2spill@gmail.com>
2016-06-13 20:52:53 -0500

    Remove old aclocal and cleanup automake directory after use.

--- get-aclocal.sh
+++ get-aclocal.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-wget http://ftp.gnu.org/gnu/automake/automake-1.5.tar.gz && \
+wget http://ftp.gnu.org/gnu/automake/automake-1.5.tar.gz &&
 tar -xvf automake-1.5.tar.gz && cd automake-1.5 && ./configure &&
-make -j2 && make install
+make -j2 && sudo rm -rf /usr/local/bin/aclocal && make install &&
+rm -rf automake-1.5.tar.gz && rm -rf automake-1.5
