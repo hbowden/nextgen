302f5eedcbc0e9d15900eb98ab969beaeb5d2cc2
nah <2trill2spill@gmail.com>
2016-06-13 21:01:08 -0500

    Revert last commit and echo aclocal version.

--- get-aclocal.sh
+++ get-aclocal.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-wget http://ftp.gnu.org/gnu/automake/automake-1.5.tar.gz &&
+wget http://ftp.gnu.org/gnu/automake/automake-1.5.tar.gz && \
 tar -xvf automake-1.5.tar.gz && cd automake-1.5 && ./configure &&
-make -j2 && sudo rm -rf /usr/local/bin/aclocal && make install &&
-rm -rf automake-1.5.tar.gz && rm -rf automake-1.5
+make -j2 && sudo make install && echo aclocal --version
