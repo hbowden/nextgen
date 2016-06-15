e8e6419aa915bc36613edd270140c7283aeba0e4
nah <2trill2spill@gmail.com>
2016-06-15 11:18:57 -0500

    Use gmake on FreeBSD.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,17 @@
 
 root=${PWD}
 
-cd $root/deps/capstone-3.0.4 && make && \
-cd $root/deps/ck-0.5.1 && ./configure && make && \
-cd $root/deps/sqlite && make && \
-cd $root/deps/libressl-2.4.1 && which aclocal && aclocal --version && ./configure && make;
+os=`uname`
+
+if [[ "$os" == 'FreeBSD' ]]; then
+	cd $root/deps/capstone-3.0.4 && gmake && \
+    cd $root/deps/ck-0.5.1 && ./configure && gmake && \
+    cd $root/deps/sqlite && gmake && \
+    cd $root/deps/libressl-2.4.1 && ./configure && gmake;
+else
+    cd $root/deps/capstone-3.0.4 && make && \
+    cd $root/deps/ck-0.5.1 && ./configure && make && \
+    cd $root/deps/sqlite && make && \
+    cd $root/deps/libressl-2.4.1 && ./configure && make;
+fi
+
