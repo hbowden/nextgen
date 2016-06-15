c292d94b7b20ed7ff9886f0794f19b0ad3bf1448
Harrison <2trill2spill@gmail.com>
2016-06-15 13:12:56 -0500

    Fix spacing.

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ os=`uname`
 echo $os
 
 if [ $os = 'FreeBSD' ]; then
-	cd $root/deps/capstone-3.0.4 && gmake && \
+    cd $root/deps/capstone-3.0.4 && gmake && \
     cd $root/deps/ck-0.5.1 && ./configure && gmake && \
     cd $root/deps/sqlite && gmake && \
     cd $root/deps/libressl-2.4.1 && ./configure && gmake;
