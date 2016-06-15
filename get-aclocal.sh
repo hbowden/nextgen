287e13b7ab33270e80b2db2dff1a87a5d02bec33
nah <2trill2spill@gmail.com>
2016-06-14 23:03:54 -0500

    Download correct automake.

--- get-aclocal.sh
+++ get-aclocal.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget http://ftp.gnu.org/gnu/automake/automake-1.5.tar.gz && \
-tar -xvf automake-1.5.tar.gz && cd automake-1.5 && ./configure &&
+wget http://ftp.gnu.org/gnu/automake/automake-1.15.tar.gz && \
+tar -xvf automake-1.15.tar.gz && cd automake-1.15 && ./configure &&
 make -j2 && sudo make install && aclocal --version
