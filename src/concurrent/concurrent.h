dd7b31473d43d38b8d6edbe20554f246d68d56ad
nah <2trill2spill@gmail.com>
2016-06-30 22:54:02 -0500

    Add new macros for handling atomic numbers and add documentation for concurrencyinterface.

--- src/concurrent/concurrent.h
+++ src/concurrent/concurrent.h
@@ -44,31 +44,66 @@ typedef struct msg_port msg_port_t;
 
 #endif
 
-#define NX_SPINLOCK_INITIALIZER CK_SPINLOCK_INITIALIZER
-
-#define NX_LIST_ENTRY(x) CK_LIST_ENTRY(x) list_entry
-#define NX_SLIST_ENTRY(x) CK_SLIST_ENTRY(x) list_entry
-
-#define NX_LIST_HEAD(name,type) 
-#define NX_SLIST_HEAD(name,type) CK_SLIST_HEAD(name, type) name
-
-#define NX_SLIST_HEAD_INITIALIZER CK_SLIST_HEAD_INITIALIZER
-
-#define NX_SLIST_INIT(list) CK_SLIST_INIT(list)
-
-#define NX_SLIST_INSERT_HEAD(pool, blk) CK_SLIST_INSERT_HEAD(pool, blk, list_entry)
-
-#define NX_SLIST_FOREACH(block, list) CK_SLIST_FOREACH(block, list, list_entry)
-
-#define NX_SLIST_REMOVE(list, blk, type) CK_SLIST_REMOVE(list, blk, type, list_entry)
-
-#define nx_spinlock_lock(lock) ck_spinlock_lock(lock)
-
-#define nx_spinlock_unlock(lock) ck_spinlock_unlock(lock)
-
 typedef ck_spinlock_t nx_spinlock_t;
 
-/* CAS loop for swapping atomic int32 values. */ 
+/**
+ *    Function like macro for initializing a spinlock at compile time.
+ *    Alternativly you can use nx_spinlock_init at runtime to initialize
+ *    a spinlock.
+ */
+#define NX_SPINLOCK_INITIALIZER CK_SPINLOCK_INITIALIZER
+
+/**
+ *    Function like macro for locking an nx_spinlock_t.
+ *    @param lock The spinlock to lock.
+ */
+#define nx_spinlock_lock(lock) ck_spinlock_lock(lock)
+
+/**
+ *    Function like macro for unlocking an nx_spinlock_t.
+ *    @param lock The spinlock to unlock.
+ */
+#define nx_spinlock_unlock(lock) ck_spinlock_unlock(lock)
+
+/**
+ *    Function like macro for atomically adding the uint32 variable pointed to by var.
+ *    @param var A pointer to a uint32 variable.
+ */
+#define atomic_add_uint32(var, val) ck_pr_add_uint32(var, val)
+
+/**
+ *    Function like macro for atomically decrementing the uint32 variable pointed to by var.
+ *    @param var A pointer to a uint32 variable.
+ */
+#define atomic_dec_uint32(var, val) ck_pr_dec_uint32(var, val)
+
+/**
+ *    Function like macro for atomically loading the value of the int32 pointed to by var and return the result.
+ *    @param var A pointer to a int32 variable to atomically load/read the value from.
+ */
+#define atomic_load_int32(var) ck_pr_load_int32(var)
+
+/**
+ *    Function like macro for atomically loading the value of the uint32 pointed to by var and return the result.
+ *    @param var A pointer to a uint32 variable to atomically load/read the value from.
+ */
+#define atomic_load_uint32(var) ck_pr_load_int32(var)
+
+#define NX_LIST_HEAD(name,type) 
+#define NX_LIST_ENTRY(x) CK_LIST_ENTRY(x) list_entry
+#define NX_SLIST_ENTRY(x) CK_SLIST_ENTRY(x) list_entry
+#define NX_SLIST_HEAD(name,type) CK_SLIST_HEAD(name, type) name
+#define NX_SLIST_HEAD_INITIALIZER CK_SLIST_HEAD_INITIALIZER
+#define NX_SLIST_INIT(list) CK_SLIST_INIT(list)
+#define NX_SLIST_INSERT_HEAD(pool, blk) CK_SLIST_INSERT_HEAD(pool, blk, list_entry)
+#define NX_SLIST_FOREACH(block, list) CK_SLIST_FOREACH(block, list, list_entry)
+#define NX_SLIST_REMOVE(list, blk, type) CK_SLIST_REMOVE(list, blk, type, list_entry)
+
+/**
+ *    A compare and swap or CAS loop. This function is for swapping atomic int32 values.
+ *    @param target A pointer to the target variable that you wan't to atomically swap.
+ *    @param value The value you wan't to replace the variable pointed to by the parameter target. 
+ */ 
 extern void cas_loop_int32(int32_t *target, int32_t value);
 
 /* CAS loop for swapping atomic uint32 values. */ 
