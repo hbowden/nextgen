07cbc1cff292f29d0644ac13130785ae11311b42
nah <2trill2spill@gmail.com>
2016-07-05 17:34:25 -0500

    Add epoch_record macro wrapper.

--- src/concurrent/concurrent.h
+++ src/concurrent/concurrent.h
@@ -50,6 +50,8 @@ typedef ck_spinlock_t nx_spinlock_t;
 
 typedef ck_epoch_t epoch_ctx;
 
+typedef ck_epoch_record_t epoch_record;
+
 /**
  *    Function like macro for initializing a spinlock at compile time.
  *    Alternativly you can use nx_spinlock_init at runtime to initialize
