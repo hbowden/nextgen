57247ed19abd3cea4bc304567fbb291faae8ddf0
nah <2trill2spill@gmail.com>
2016-08-05 20:46:00 -0500

    Update helper scripts for the libressl update.

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ echo $os
 if [ $os = 'FreeBSD' ]; then
     cd $root/../deps/capstone-3.0.4 && gmake install && \
     cd $root/../deps/ck-0.5.1 && gmake install && \
-    cd $root/../deps/libressl-2.4.1 && gmake install;
+    cd $root/../deps/libressl-2.4.2 && gmake install;
 else
     cd $root/../deps/capstone-3.0.4 && make install && \
     cd $root/../deps/ck-0.5.1 && make install && \
-    cd $root/../deps/libressl-2.4.1 && make install;
+    cd $root/../deps/libressl-2.4.2 && make install;
 fi
--- setup.sh
+++ setup.sh
@@ -10,11 +10,11 @@ if [ $os = 'FreeBSD' ]; then
     cd $root/deps/capstone-3.0.4 && gmake && \
     cd $root/deps/ck-0.5.1 && ./configure && gmake && \
     cd $root/deps/sqlite && gmake && \
-    cd $root/deps/libressl-2.4.1 && ./configure && gmake;
+    cd $root/deps/libressl-2.4.2 && ./configure && gmake;
 else
     cd $root/deps/capstone-3.0.4 && make && \
     cd $root/deps/ck-0.5.1 && ./configure && make && \
     cd $root/deps/sqlite && make && \
-    cd $root/deps/libressl-2.4.1 && ./configure && make;
+    cd $root/deps/libressl-2.4.2 && ./configure && make;
 fi
 
