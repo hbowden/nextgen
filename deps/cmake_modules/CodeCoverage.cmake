a39016a75e422e89b386014677e287d94bd6d184
nah <2trill2spill@gmail.com>
2016-08-10 22:02:56 -0500

    Remove clang check because it fails on osx.
    Starting with cmake 3.0.0 the clang on osx has the id is AppleClang instead of
    Clang. Remove check so coverage works on osx.

--- deps/cmake_modules/CodeCoverage.cmake
+++ deps/cmake_modules/CodeCoverage.cmake
@@ -81,9 +81,6 @@ IF(NOT CMAKE_COMPILER_IS_GNUCXX)
 	# Clang version 3.0.0 and greater now supports gcov as well.
 	MESSAGE(WARNING "Compiler is not GNU gcc! Clang Version 3.0.0 and greater supports gcov as well, but older versions don't.")
 
-	IF(NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-		MESSAGE(FATAL_ERROR "Compiler is not GNU gcc! Aborting...")
-	ENDIF()
 ENDIF() # NOT CMAKE_COMPILER_IS_GNUCXX
 
 SET(CMAKE_CXX_FLAGS_COVERAGE
