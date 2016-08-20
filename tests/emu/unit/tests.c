e9733f5fe02ef91328ae3832f883c53a45a86098
nah <2trill2spill@gmail.com>
2016-08-19 20:26:31 -0500

    add emu unit tests.

--- tests/emu/unit/tests.c
+++ tests/emu/unit/tests.c
@@ -15,8 +15,23 @@
 
 #include "unity.h"
 
-/* Include the C source file so we can test internals. */
+/* Include the C source file so we can test opaque structs. */
 #include "../../../src/emu/emu.c"
+
+static void test_free_emulator(void)
+{
+    struct emulator_ctx *emu = NULL;
+
+    emu = init_emulator();
+    TEST_ASSERT_NOT_NULL(emu);
+
+    free_emulator(&emu);
+
+    /* Should be set to NULL after calling free_emulator(). */
+    TEST_ASSERT_NULL(emu);
+
+    return;
+}
 
 static void test_init_emulator(void)
 {
@@ -24,13 +39,44 @@ static void test_init_emulator(void)
 
     emu = init_emulator();
     TEST_ASSERT_NOT_NULL(emu);
+    TEST_ASSERT_NOT_NULL(&emu->epoch);
 
     return;
+}
+
+static void test_load_file(void)
+{
+   struct emulator_ctx *emu = NULL;
+
+   emu = init_emulator();
+   TEST_ASSERT_NOT_NULL(emu);
+
+   /* Load_file() should fail when given a bad file path. */
+   int32_t rtrn = load_file(emu, "bogus/file/path");
+   TEST_ASSERT(rtrn == -1);
+
+   /* Load_file() should fail when given a NULL path. */
+   rtrn = load_file(emu, NULL);
+   TEST_ASSERT(rtrn == -1);
+
+   /* Load_file() should return zero when given a valid executable. */
+   rtrn = load_file(emu, "../tests/emu/helper_files/test");
+   TEST_ASSERT(rtrn == 0);
+   TEST_ASSERT_NOT_NULL(emu->program_data);
+
+   free_emulator(&emu);
+
+   /* Should be set to NULL after calling free_emulator(). */
+   TEST_ASSERT_NULL(emu);
+
+   return;
 }
 
 int main(void)
 {
     test_init_emulator();
+    test_free_emulator();
+    test_load_file();
 
     return (0);
 }
