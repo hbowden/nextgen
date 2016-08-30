66b1fab83592ea584b15bc0ef7b76b31d3c12105
nah <2trill2spill@gmail.com>
2016-08-30 11:18:37 -0500

    Update to concurrencykit 0.5.2.

--- setup.sh
+++ setup.sh
@@ -3,18 +3,18 @@
 root=${PWD}
 
 os=`uname`
+ck='ck-0.5.2'
 
 echo $os
 
 if [ $os = 'FreeBSD' ]; then
     cd $root/deps/capstone-3.0.4 && gmake && \
-    cd $root/deps/ck-0.5.1 && ./configure && gmake && \
+    cd $root/deps/$ck && ./configure && gmake && \
     cd $root/deps/sqlite && gmake && \
     cd $root/deps/libressl-2.4.2 && ./configure && gmake;
 else
     cd $root/deps/capstone-3.0.4 && make && \
-    cd $root/deps/ck-0.5.1 && ./configure && make && \
+    cd $root/deps/$ck && ./configure && make && \
     cd $root/deps/sqlite && make && \
     cd $root/deps/libressl-2.4.2 && ./configure && make;
 fi
-
