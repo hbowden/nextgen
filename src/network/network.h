ff63ea6be7c21e0b09ac7387bf2b36565ecf30eb
nah <2trill2spill@gmail.com>
2016-10-22 14:12:08 -0500

    Deprecate old interfaces and mark private interfaces as private.

--- src/network/network.h
+++ src/network/network.h
@@ -2,15 +2,15 @@
 
 /**
  * Copyright (c) 2015, Harrison Bowden, Minneapolis, MN
- * 
+ *
  * Permission to use, copy, modify, and/or distribute this software for any purpose
- * with or without fee is hereby granted, provided that the above copyright notice 
+ * with or without fee is hereby granted, provided that the above copyright notice
  * and this permission notice appear in all copies.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH 
- * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
  * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
- * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, 
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
  * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
  * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  **/
@@ -19,21 +19,21 @@
 #define NETWORK_H
 
 #include <stdint.h>
+#include "utils/private.h"
+#include "utils/deprecate.h"
 
 enum network_mode { SOCKET_SERVER };
 
-extern int32_t setup_network_module(enum network_mode mode);
+extern DEPRECATED int32_t setup_network_module(enum network_mode mode);
 
-extern void get_server_port(uint32_t *port);
+extern private void get_server_port(uint32_t *port);
 
-extern int32_t pick_random_port(uint32_t *port);
+extern DEPRECATED int32_t connect_ipv4(int32_t *sockFd);
 
-extern int32_t connect_ipv4(int32_t *sockFd);
+extern DEPRECATED int32_t connect_ipv6(int32_t *sockFd);
 
-extern int32_t connect_ipv6(int32_t *sockFd);
+extern private int32_t setup_ipv4_tcp_server(int32_t *sockFd);
 
-extern int32_t setup_ipv4_tcp_server(int32_t *sockFd);
-
-extern int32_t setup_ipv6_tcp_server(int32_t *sockFd);
+extern private int32_t setup_ipv6_tcp_server(int32_t *sockFd);
 
 #endif
