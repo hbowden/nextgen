4af825dcf292d178e185f53232ec6187c284abdf
nah <2trill2spill@gmail.com>
2016-10-27 01:03:30 -0500

    Remove legacy code.

--- src/main.m
+++ src/main.m
@@ -23,10 +23,10 @@ int main(int argc, const char * argv[])
 {
     int32_t rtrn = 0;
     struct parser_ctx *ctx = NULL;
-    //struct fuzzer *nextgen = NULL;
     struct output_writter *output = NULL;
     struct memory_allocator *allocator = NULL;
 
+    /* Use the console/terminal for our output device. */
     output = get_console_writter();
     if(output == NULL)
     {
@@ -35,17 +35,11 @@ int main(int argc, const char * argv[])
         return (-1);
     }
 
+    /* Get an allocator object so we can allocate other objects in our program. */
     allocator = get_default_allocator();
     if(allocator == NULL)
     {
         output->write(ERROR, "Failed to get memory allocator\n");
-        return (-1);
-    }
-
-    map = allocator->shared(sizeof(struct shared_map));
-    if(map == NULL)
-    {
-        output->write(ERROR, "Can't create shared mapping.\n");
         return (-1);
     }
 
@@ -73,47 +67,6 @@ int main(int argc, const char * argv[])
         output->write(ERROR, "Can't initialize.\n");
         return (-1);
     }
-
-    // /* Get selected fuzzer. */
-    // nextgen = get_fuzzer(map, output);
-    // if(nextgen == NULL)
-    // {
-    //     output->write(ERROR, "Failed to initialize fuzzer");
-    //     return (-1);
-    // }
-    //
-    // /* If we are file fuzzing on OSX we need to use objective c
-    // and as a consequence we need to setup the objc run loop. */
-    // if(map->mode == MODE_FILE)
-    // {
-    //     /* Pass the objective c runtime our setup and start functions. */
-    //     rtrn = setup_objc_runtime((int32_t (*)(void *))&setup_runtime,
-    //                               &start_runtime, map);
-    //     if(rtrn < 0)
-    //     {
-    //        output->write(ERROR, "Can't setup objective-c runtime\n");
-    //        return (-1);
-    //     }
-    // }
-    // else
-    // {
-    //     /* Setup the fuzzer running enviroment. */
-    //     rtrn = setup_runtime(map);
-    //     if(rtrn < 0)
-    //     {
-    //         output->write(ERROR, "Can't setup runtime enviroment.\n");
-    //         return (-1);
-    //     }
-    //
-    //     /* Start the main fuzzing loop. */
-    //     rtrn = start_runtime();
-    //     if(rtrn < 0)
-    //     {
-    //         output->write(ERROR, "Can't start runtime enviroment.\n");
-    //         clean_shared_mapping();
-    //         return (-1);
-    //     }
-    // }
 
     /* We should only reach here on ctrl-c. */
     clean_shared_mapping();
