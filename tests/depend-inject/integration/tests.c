21f929a8834f2597bfb48a9fd0896ae4cb494b40
nah <2trill2spill@gmail.com>
2016-12-11 15:08:41 -0600

    Create tests add_deps().

--- tests/depend-inject/integration/tests.c
+++ tests/depend-inject/integration/tests.c
@@ -18,6 +18,7 @@
 #include "utils/utils.h"
 #include "utils/autofree.h"
 #include "crypto/crypto.h"
+#include "crypto/random.h"
 #include "memory/memory.h"
 #include "depend-inject/depend-inject.h"
 
@@ -68,11 +69,52 @@ static void test_create_dependency_array(void)
     return;
 }
 
+static void test_add_dep(void)
+{
+    struct dependency_context *ctx = NULL;
+    struct output_writter *output = NULL;
+
+    output = get_console_writter();
+    TEST_ASSERT_NOT_NULL(output);
+
+    struct memory_allocator *allocator = NULL;
+
+    allocator = get_default_allocator();
+    TEST_ASSERT_NOT_NULL(allocator);
+
+    ctx = create_dependency_ctx(create_dependency(output, OUTPUT),
+                                create_dependency(allocator, ALLOCATOR),
+                                NULL);
+    TEST_ASSERT_NOT_NULL(ctx->array);
+    TEST_ASSERT(ctx->count == 2);
+    uint32_t i;
+    for(i = 0; i < ctx->count; i++)
+    {
+        TEST_ASSERT_NOT_NULL(ctx->array[i]);
+    }
+
+    TEST_ASSERT(ctx->array[0]->name == OUTPUT);
+    TEST_ASSERT(ctx->array[1]->name == ALLOCATOR);
+
+    inject_crypto_deps(ctx);
+
+    struct random_generator *random_gen = NULL;
+    random_gen = get_default_random_generator();
+    TEST_ASSERT_NOT_NULL(random_gen);
+
+    int32_t rtrn = 0;
+
+    rtrn = add_dep(ctx, create_dependency(random_gen, RANDOM_GEN));
+    TEST_ASSERT(rtrn == 0);
+    TEST_ASSERT(ctx->array[2]->name == RANDOM_GEN);
+}
+
 
 int main(void)
 {
     test_create_dependency();
     test_create_dependency_array();
+    test_add_dep();
 
     _exit(0);
 }
