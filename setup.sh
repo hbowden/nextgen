85a5d19459bd0eba4b6f2761e1b26324fc213c60
nah <2trill2spill@gmail.com>
2017-03-09 17:36:32 -0600

    Update setup script.

--- setup.sh
+++ setup.sh
@@ -3,18 +3,18 @@
 root=${PWD}
 
 os=`uname`
-ck='ck-0.5.2'
+ck='ck-0.6.0'
 
 echo $os
 
 if [ $os = 'FreeBSD' ]; then
-    cd $root/deps/capstone-3.0.4 && gmake && \
+    cd $root/deps/capstone-3.0.5-rc2 && gmake && \
     cd $root/deps/$ck && ./configure && gmake && \
     cd $root/deps/sqlite && gmake && \
-    cd $root/deps/libressl-2.5.0 && ./configure && gmake;
+    cd $root/deps/libressl-2.5.1 && ./configure && gmake;
 else
-    cd $root/deps/capstone-3.0.4 && make && \
+    cd $root/deps/capstone-3.0.5-rc2 && make && \
     cd $root/deps/$ck && ./configure && make && \
     cd $root/deps/sqlite && make && \
-    cd $root/deps/libressl-2.5.0 && ./configure && make;
+    cd $root/deps/libressl-2.5.1 && ./configure && make;
 fi
